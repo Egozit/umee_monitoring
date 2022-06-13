1c01640f01ed95977996b6eb4ddb925c355dcc3c
Egor Petrov <iampetroved@gmail.com>
2022-06-13 19:11:44 +0300

    Update mon_var_template.sh

--- mon_var_template.sh
+++ mon_var_template.sh
@@ -1,6 +1,6 @@
 #UMEE monitoring variables template
-MON_MODE=rpc   # use for RPC/sentry nodes
-#MON_MODE=val   # use for Validator nodes
+#MON_MODE=rpc   # use for RPC/sentry nodes
+MON_MODE=val   # use for Validator nodes
 COS_BIN_NAME=insert_path_to_umee_binary # example: /root/go/bin/umeed or /home/user/go/bin/umeed
 COS_DENOM=uumee  # umee denominator. don't change
 COS_PORT_RPC=26657 # insert node RPC port here if it's not default (26657)
